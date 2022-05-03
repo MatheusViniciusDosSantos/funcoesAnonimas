890fabab90d3421279c4d425f4f41ac12ef787cb
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-05-03 19:56:20 -0300

    Adicionada interface de depuração

--- funcoes_anonimas/bin/funcoes_anonimas.dart
+++ funcoes_anonimas/bin/funcoes_anonimas.dart
@@ -1,8 +1,10 @@
 import '../lib/funcoes.dart' as funcoes;
 import '../lib/acoesSalario.dart' as salario;
+import '../lib/interface.dart' as interfaces;
 
 void main(List<String> arguments) {
   // funcoes.mostrarNaTela();
   // funcoes.show();
-  salario.show();
+  // salario.show();
+  interfaces.show();
 }
